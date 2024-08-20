4559807406c93d22ce9e37ab6acf4318ca45aebf
Georgy Khomutov <xomytob@list.ru>
2024-08-20 21:47:07 +0300

    small changes

--- Algo/Number_Theory/Eratosthenes_sieve.cpp
+++ Algo/Number_Theory/Eratosthenes_sieve.cpp
@@ -11,24 +11,28 @@ vector<bool> isprime(N + 1, true);
 void sieve()
 {
     isprime[0] = isprime[1] = false;
-    for (int j = 4; j <= N; j += 2)
+    for (int j = 4; j <= N; j += 2) {
         isprime[j] = false;
-    for (int i = 3; i*i <= N; i += 2)
-        if (isprime[i])
-            for (int j = i*i; j <= N; j += i)
+    }
+    for (int i = 3; i*i <= N; i += 2) {
+        if (isprime[i]) {
+            for (int j = i*i; j <= N; j += i) {
                 isprime[j] = false;
+            }
+        }
+    }
 }
 
 
 /* Linear + factorization */
-vector<int> min_d(N + 1, 0);
+int min_d[N + 1];
 vector<int> pr; // replace with array for speed
 void seive() {
     for (int i=2; i<=N; ++i) {
-    	if (min_d[i] == 0) {
-    		min_d[i] = i;
-    		pr.push_back(i);
-    	}
+        if (min_d[i] == 0) {
+            min_d[i] = i;
+            pr.push_back(i);
+        }
         for (int p : pr) {
             ull ip = ull(i) * p;
             if (ip > N || p > min_d[i])
