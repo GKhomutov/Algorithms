40f2a9e72cbeced9324b2d014b216a8e7b64fe51
Georgy Khomutov <xomytob@list.ru>
2024-08-20 21:47:52 +0300

    small changes

--- Algo/SegTree/Upd_element.cpp
+++ Algo/SegTree/Upd_element.cpp
@@ -13,24 +13,25 @@ struct SegTree
     SegTree(int sz, const Fun &g, T default_value = T()) : f(g)
     {
         n = 1;
-        while (n < sz)
-            n <<= 1;
-        t.resize(n * 2, default_value);
+        while (n < sz) n <<= 1;
+        t.resize(2 * n, default_value);
     }
 
     SegTree(vector<T> &a, const Fun &g, T default_value = T()) : SegTree(a.size(), g, default_value)
     {
-        copy(all(a), t + n);
-        for (int i = n - 1; i != 0; --i)
+        copy(all(a), t.begin() + n);
+        for (int i = n - 1; i != 0; --i) {
             t[i] = f(t[i << 1], t[i << 1 | 1]);
+        }
     }
 
     void upd(int i, const T &x)
     {
         i += n;
         t[i] = f(t[i], x);
-        for (i >>= 1; i != 0; i >>= 1)
+        for (i >>= 1; i != 0; i >>= 1) {
             t[i] = f(t[i << 1], t[i << 1 | 1]);
+        }
     }
     
     // [l, r)
@@ -38,10 +39,12 @@ struct SegTree
     {
         T resL = t[0], resR = t[0];
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l & 1)
+            if (l & 1) {
                 resL = f(resL, t[l++]);
-            if (r & 1)
+            }
+            if (r & 1) {
                 resR = f(t[--r], resR);
+            }
         }
         return f(resL, resR);
     }
